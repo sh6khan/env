e88afc5a45054bab84695f815106a1d1fa89076a
sh6khan <sadman.khan999@gmail.com>
2016-03-27 22:46:31 -0400

    adding an external aliases file and prepping the directory structure

---
+++ setup_directory_structure.rb
@@ -0,0 +1,8 @@
+# this script is used to create the directory for my personal setup
+# The idea would be that this repo would be cloned into the home directory of the user
+#
+# all the aliases and profile will exist in this project. Therfore when we link
+# aliases in the directory to the .zshrc file it will have a path come directly 
+# to the aliases file in this directory
+
+# TODO : everything else .. :(
