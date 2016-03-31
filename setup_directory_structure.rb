1e47d2aa049e44ec28630d5e287f20f1ea799302
sh6khan <sadman.khan999@gmail.com>
2016-03-30 23:45:11 -0400

    added my vimrc and setup for vundle and pathogen use

--- setup_directory_structure.rb
+++ setup_directory_structure.rb
@@ -1,4 +1,4 @@
-# this script is used to create the directory for my personal setup
+# this script is used to create the directory structure for my personal setup
 # The idea would be that this repo would be cloned into the home directory of the user
 #
 # all the aliases and profile will exist in this project. Therfore when we link
@@ -6,3 +6,60 @@
 # to the aliases file in this directory
 
 # TODO : everything else .. :(
+
+
+require 'fileutils'
+
+
+def run(cmd)
+  unless system(cmd)
+    abort("Command `#{cmd}` failed with status #{$?.exitstatus}")
+  end
+end
+
+# import aliases
+def import_aliases
+  zshrc = File.join(ENV['HOME'], '.zshrc')
+
+  unless File.read(zshrc).include?('/env/aliases')
+    File.write(zshrc, "source $HOME/env/aliases\n", mode: 'a')
+  end
+end
+
+
+# import .vimrc file
+def import_vimrc
+
+  vimrc = File.join(ENV['HOME'], '.vimrc');
+
+  unless File.read(vimrc).include?('/env/vimrc')
+    File.write(vimrc, "source $HOME/env/vimrc\n", mode: 'a')
+  end
+end
+
+
+# plugin installer for vim
+def installing_pathogen
+  run('mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+      curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim')
+end
+
+# vundle is an another plugin manager
+# inspired by ruby bundler
+#
+def installing_vundle
+  run("git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim")
+end
+
+
+# import config files
+import_aliases
+import_vimrc
+
+# instal pathogenfor plugins
+installing_pathogen
+
+# instsall plugins
+
+
+
